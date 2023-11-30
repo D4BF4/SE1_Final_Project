f5fe9b793df7407ae59321ad4279b14960495c01
Riad-Jaber <145401190+Riad-Jaber@users.noreply.github.com>
2023-11-30 14:06:35 -0500

    Update Riad's-Code.cpp

--- Riad's-Code.cpp
+++ Riad's-Code.cpp
@@ -10,11 +10,6 @@ public:
     // Constructor
     Player(int startX, int startY, int initialHealth) : xPos(startX), yPos(startY), health(initialHealth) {}
 
-    // Getter for player position
-    std::pair<int, int> providePosition() const {
-        return std::make_pair(xPos, yPos);
-    }
-
     // Method to handle player movement
     void moveDirection(int xDelta, int yDelta) {
         // Add logic to check boundaries or obstacles before moving
@@ -30,14 +25,10 @@ public:
         // Add logic for shooting, such as creating projectiles or affecting enemies
     }
 
-    // Method to handle player taking damage
-    void takeDamage(int damageAmount) {
-        health -= damageAmount;
-
-        // Add logic for checking if the player is defeated
-        if (health <= 0) {
-            // Player is defeated, handle accordingly (e.g., end the game)
-        }
+    // Method to provide player position verbally
+    void providePosition() const {
+        std::cout << "Player Position: (" << xPos << ", " << yPos << ")\n";
+        // You can add additional logic here for verbalizing the position, e.g., using text-to-speech
     }
 
     // Additional methods or features can be added based on your game requirements
@@ -48,12 +39,7 @@ int main() {
     Player player(0, 0, 100);  // Starting position (0, 0) with 100 health
     player.moveDirection(1, 1);  // Move the player
     player.shoot();  // Player shoots
-    player.takeDamage(20);  // Player takes damage
-
-    // Display player position
-    std::pair<int, int> playerPosition = player.providePosition();
-    std::cout << "Player Position: (" << playerPosition.first << ", " << playerPosition.second << ")\n";
+    player.providePosition();  // Verbally provide player position
 
     return 0;
 }
-
